08e55f9b98479c3d7c1b9d669f3e1b0cc426d583
Wisleymiguel <wisleymiguel4032x@gmail.com>
2025-08-29 15:24:58 -0300

    api com K6

---
+++ k6-ebac-test/user.test.js
@@ -0,0 +1,14 @@
+import http from "k6/http";
+import { group, sleep } from "k6";
+import Login from "../request/login.request";
+import data from "../data/usuarios.json";
+
+export default function () {
+  let login = new Login();
+
+  group("login and get token", () => {
+    login.access(data.usuarioOk.user, data.usuarioOk.pass);
+  });
+
+  group("list users", () => {});
+}
